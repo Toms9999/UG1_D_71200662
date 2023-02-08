dba2326472d88f8d1fe58672b92f53fdeb656efb
Thomas Janir <117893395+Toms9999@users.noreply.github.com>
2023-02-08 18:48:23 +0700

    Update Soal2_D_71200662.py

--- Soal2_D_71200662.py
+++ Soal2_D_71200662.py
@@ -1,9 +1,7 @@
-a = str(input("Kalimat yang ingin diteliti : "))
-b = str(input("Kata yang di cari : "))
+a = str(input('Kalimat yang ingin diteliti : '))
+b = str(input('Kata yang di cari : '))
 
-for b in a :
+for i in a :
     a = a.count(b)
-
-print("\nJumlah kata yang di cari :", a)
-
-
+    print('\nJumlah kata yang di cari :', a)
+    break
